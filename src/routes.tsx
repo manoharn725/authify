0cd3912fba12f132059829983ca70184e1c7c863
manoharn725 <manoharnmanu725@gmail.com>
2025-04-09 13:14:52 +0530

    refactor: removed lazy loading for ProtectedRoutes and ProtectedLayout

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,28 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import { lazy, Suspense } from "react";
 import Loader from "./components/Loader";
+import ProtectedRoutes from "./components/ProtectdRoutes";
+import ProtectedLayout from "./components/ProtectedLayout";
 
 //Lazy Loading..
 const Home = lazy(() => import("./pages/Home"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-const ProtectedRoutes = lazy(() => import("./components/ProtectdRoutes"));
-const ProtectedLayout = lazy(() => import("./components/ProtectedLayout"));
 
 export const router = createBrowserRouter([
   {
-    element: (
-      <Suspense fallback={<Loader />}>
-        <ProtectedRoutes />
-      </Suspense>
-    ),
+    element: <ProtectedRoutes />,
     children: [
       {
-        element: (
-          <Suspense fallback={<Loader />}>
-            <ProtectedLayout />
-          </Suspense>
-        ),
+        element: <ProtectedLayout />,
         children: [
           {
             path: "/",
