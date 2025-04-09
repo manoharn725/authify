3e95128fb090d116a598f77b2eaf0019602f25ac
manoharn725 <manoharnmanu725@gmail.com>
2025-04-09 13:13:55 +0530

    feat: implemented lazy loading with Lottie loader animation

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,6 +1,12 @@
+import Lottie from "lottie-react";
+import AutifyLoader from "../../assets/authifyLoader.json";
 // interface ILoader{}
 
 const Loader: React.FunctionComponent = () => {
-    return( <div className="text-center p-4">Loading...</div>)
-}
+  return (
+    <div className="min-h-[100dvh] flex items-center w-[300px] mx-auto">
+      <Lottie animationData={AutifyLoader} loop autoPlay />
+    </div>
+  );
+};
 export default Loader;
